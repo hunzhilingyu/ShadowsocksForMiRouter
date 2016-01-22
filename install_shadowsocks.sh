7ea14bf63ddf50acfc1bb39b72da9da70118886a
terrychan <terry@iMac.local>
2016-01-22 21:13:44 +0800

    fix bug

--- install_shadowsocks.sh
+++ install_shadowsocks.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-firewallBackupFile="/etc/firewall.user.back"
-
 clear
 echo "#############################################################"
 echo "# Install Shadowsocks for Miwifi(r1d)"
 echo "#############################################################"
 
 # Check wheather Shadowsocks has been installed
-if [[ -d "$firewallBackupFile" ]]; then
+if [[ -f /etc/firewall.user.back ]]; then
 	echo "Error: You have installed Shadowsocks. Please remove it after update the MiRouter!" 1>&2
 	exit 1
 fi
